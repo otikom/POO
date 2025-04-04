d7f7ea5bc09e43f64795e6edeff2ba2bed4f8a5d
Maria Jose Sanchez <samaria@uninorte.edu.co>
2025-04-03 19:25:10 -0500

    corregirlo ekisde

--- ola/src/main/Main.java
+++ ola/src/main/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ola;
+package main;
 
 /**
  *
  * @author maria
  */
-public class Ola {
+public class Main {
 
     /**
      * @param args the command line arguments
